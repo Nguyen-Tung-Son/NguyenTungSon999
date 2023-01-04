640332506388eec660b6cb7c2445881351567c04
Nguyen Tung Son <titlai434321@gmail.com>
2023-01-04 08:10:08 +0700

    first_commit_NTS999

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace NguyenTungSon999.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
